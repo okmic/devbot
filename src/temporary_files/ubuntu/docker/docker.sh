13c66eb1764b2237f14e8cd4556777171ed6b637
okmic <okmic.g@gmail.com>
2024-11-29 10:39:05 +0300

    docker

---
+++ src/temporary_files/ubuntu/docker/docker.sh
@@ -0,0 +1,5 @@
+sudo apt update
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
