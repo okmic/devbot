7607896040a79374ec01e734ea1258692a2fd425
Okhtov Michael <80267051+okmic@users.noreply.github.com>
2025-08-13 19:48:39 +0300

    Update docker.sh

--- src/temporary_files/ubuntu/docker/docker.sh
+++ src/temporary_files/ubuntu/docker/docker.sh
@@ -1,5 +1,19 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker --version
+docker-compose --version
