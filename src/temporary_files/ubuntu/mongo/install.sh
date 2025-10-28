e6cbb0d66821e2b905982ed382c35a8a41bb6077
Okhtov Michael <80267051+okmic@users.noreply.github.com>
2025-10-28 21:57:13 +0300

    Update install.sh

--- src/temporary_files/ubuntu/mongo/install.sh
+++ src/temporary_files/ubuntu/mongo/install.sh
@@ -5,16 +5,14 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-# Установка необходимых пакетов
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl gnupg -y
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-
-echo "Установка необходимых пакетов..."
-apt update
-sudo apt install mongodb-org
-mongod --version
-
+sudo apt update
+sudo apt install mongodb-org -y
 sudo systemctl start mongod
 sudo systemctl enable mongod
+sudo systemctl status mongod
 
-echo "MongoDB успешно установлен и запущен!"
+echo "Success"
